2d6624bc9d60b0c5b043b32d415670e353831bf9
jzhang182 <62226250+jzhang182@users.noreply.github.com>
2020-04-08 20:47:45 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace HealthInfo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            MainForm mainForm = new MainForm();
+            Application.Run(mainForm);
         }
     }
 }
